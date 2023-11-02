330f7a85748c70dd533aee4856b60b7187de3dec
asifbhuiyann <getconnectwithasif@gmail.com>
2023-11-02 11:02:40 +0600

    Contains nav content

---
+++ nav.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    header {
+        background-color: #2e4d1d;
+        color: #fff;
+        padding: 20px 0;
+    }
+
+    nav ul {
+        list-style: none;
+        display: flex;
+        justify-content: center;
+    }
+
+    nav li {
+        margin: 0 20px;
+    }
+
+    nav a {
+        text-decoration: none;
+        color: #fff;
+        font-weight: bold;
+    }
+</style>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="homepage.php">হোমপেজ</a></li>
+                <li><a href="about.php">আমাদের সম্পর্কে</a></li>
+                <li><a href="service.php">আমাদের সেবা</a></li>
+                <li><a href="favourite.php">প্রিয় পণ্য</a></li>
+                <li><a href="user_profile.php">প্রোফাইল</a></li>
+                <li><a href="feedback.php">প্রতিক্রিয়া জানান</a></li>
+                <li><a href="https://weather-six-theta.vercel.app/?fbclid=IwAR02yOudHnHDnmagcdeuMwOPBkppIL_2s4s9v5CLYIn_XrnxryJpzPXVO6U">আবহাওয়ার পূর্বাভাস</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+</body>
+
+</html>

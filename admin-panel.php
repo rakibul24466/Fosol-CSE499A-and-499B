99baf02bffe0bf7bc99d5caba5b4730999bc0955
asifbhuiyann <getconnectwithasif@gmail.com>
2023-12-17 20:00:32 +0600

    Admin panel structre design complete

---
+++ admin-panel.php
@@ -0,0 +1,131 @@
+<?php
+include 'dbconnect.php';
+session_start();
+$email = $_SESSION['user_data']['email'];
+
+$sql = "SELECT full_name FROM admin WHERE email = '$email'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+    while ($row = $result->fetch_assoc()) {
+        $Name = $row["full_name"];
+    }
+} else {
+    echo "No results for the current user";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+</head>
+<style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff !important;
+            padding: 10px;
+            text-align: center;
+        }
+        header a{
+            text-decoration: none;
+            color: white;
+        }
+        nav {
+            background-color: #555;
+            color: #fff;
+            padding: 10px;
+            text-align: center;
+            width: 200px;
+            position: fixed;
+            height: 100%;
+        }
+
+        nav a {
+            display: block;
+            color: #fff;
+            text-decoration: none;
+            padding: 10px;
+            margin: 0 0 5px;
+        }
+        nav a:hover {
+            transform: translateY(-5px);
+            font-size: 20px;
+            color: tomato;
+        }
+        section {
+            margin-left: 220px;
+            padding: 20px;
+        }
+
+        h2 {
+            color: #333;
+        }
+        .dashboard p{
+            color: black;
+            text-align: center;
+            font-size: 30px;
+            text-decoration: none;
+            font-weight: bold;
+        }
+        p{
+            color: black;
+            text-align: center;
+            font-size: 30px;
+            text-decoration: none;
+        }
+        .total-products p{
+            text-decoration: none;
+        }
+        .red-container {
+            background-color: red;
+            padding: 20px;
+            margin-top: 20px;
+            margin-left: 270px;
+            width: 400px;
+            border-radius: 20px;
+        }
+        .red-container p{
+            text-decoration: none;
+            color: white;
+        }
+        .green-container {
+            background-color: green;
+            padding: 20px;
+            margin-top: 20px;
+            margin-left: 270px;
+            width: 400px;
+            border-radius: 20px;
+        }
+        .green-container p{
+            text-decoration: none;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+
+    <header>
+        <a href="admin-panel.php"><h1>Admin Panel</h1></a>
+        <h3 style="text-align: center;">Welcome, <?php echo $Name; ?></h3><br>
+    </header>
+
+    <nav>
+        <a href="user_details.php">Users</a>
+        <a href="products.php">Products</a>
+        <a href="orders.php">Orders</a>
+        <a href="logout.php">Logout</a>
+    </nav>
+       
+</body>
+</html>
+</html>

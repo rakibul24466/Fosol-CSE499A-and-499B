f0f0baa10f783145bea543c38e6a25970995c5e8
asifbhuiyann <getconnectwithasif@gmail.com>
2024-03-31 19:02:50 +0600

    removed comments

--- dbconnect.php
+++ dbconnect.php
@@ -4,10 +4,8 @@ $username = "root";
 $password = "";
 $database = "fosol";
 
-// Create a connection to the database
 $conn = mysqli_connect($servername, $username, $password, $database);
 
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }

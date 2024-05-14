cf1076cfdf5ee88958c90a43352d55d67207eb7d
asifbhuiyann <getconnectwithasif@gmail.com>
2024-05-14 19:03:39 +0600

    final tuning complete

--- editinfo.php
+++ editinfo.php
@@ -39,7 +39,6 @@ if (isset($_POST['update'])) {
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
     <style>

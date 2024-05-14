09597ffe03b9337daf2e94674738d4d29db7eb66
asifbhuiyann <getconnectwithasif@gmail.com>
2024-05-14 16:09:49 +0600

    Modified for viewing live search products

--- search.php
+++ search.php
@@ -3,13 +3,15 @@ include 'dbconnect.php';
 $output = '';
 if (isset($_POST["query"])) {
     $search = mysqli_real_escape_string($conn, $_POST["query"]);
-    $query = "SELECT item_name FROM products WHERE item_name LIKE '%" . $search . "%'";
+    $query = "SELECT item_name, img_src FROM products WHERE item_name LIKE '%" . $search . "%'";
     $result = mysqli_query($conn, $query);
     $output = '<ul class="list-unstyled">';
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_array($result)) {
             $item_name = $row["item_name"];
-            $output .= '<li><a href=" ">' . $item_name . '</a></li>';
+            $img_src = $row["img_src"];
+            $img_src = urlencode($img_src);
+            $output .= '<li data-img-src="' . $img_src . '"><a href="#">' . $item_name . '</a></li>';
         }
     } else {
         $output .= '<li>Product Not Found</li>';
